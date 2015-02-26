fb12924db6b33afe2d107763f007720771a95845
Marshall Bowers <elliott.codes@gmail.com>
2015-02-26 15:42:14 -0500

    Add editing capabilities

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var express = require('express');
+var bodyParser = require('body-parser');
 var path = require('path');
 var fs = require('fs');
 var jade = require('jade');
@@ -10,15 +11,22 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 var router = express.Router();
 
-router.route('/').get(function (req, res) {
-    var options = {};
-    var globals = {
-        content: 'Some substituted content.'
-    };
+var options = {};
+var globals = {
+    content: 'Some substituted content.'
+};
 
+router.route('/').get(function (req, res) {
     fs.readFile('./page.jade', 'utf8', function (err, data) {
+        if (err) {
+            throw err;
+        }
+
         var compileJade = jade.compile(data);
 
         var locals = {
@@ -29,6 +37,38 @@ router.route('/').get(function (req, res) {
         return res.render('editor', locals, function (err, data) {
             return res.send(data);
         });
+    });
+});
+
+router.route('/render').get(function (req, res) {
+    fs.readFile('./page.jade', 'utf8', function (err, data) {
+        if (err) {
+            throw err;
+        }
+
+        var compileJade = jade.compile(data);
+
+        return res.send(compileJade(globals));
+    });
+});
+
+router.route('/update').post(function (req, res) {
+    var source = req.body.source;
+
+    var compileJade = jade.compile(source);
+
+    try {
+        compileJade(globals);
+    } catch (e) {
+        throw e;
+    }
+
+    fs.writeFile('./page.jade', source, function (err) {
+        if (err) {
+            throw err;
+        }
+
+        return res.redirect('/');
     });
 });
 
