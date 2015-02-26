8bb142cbb1cb563fff6501a5ebb6d17823f632bd
Marshall Bowers <elliott.codes@gmail.com>
2015-02-26 14:49:59 -0500

    Render jade from file

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
 'use strict';
 
+var express = require('express');
+var path = require('path');
+var fs = require('fs');
 var jade = require('jade');
 
-var options = {};
-var locals = {
-    content: 'Some substituted content.'
-};
+var app = express();
 
-var compile = jade.compileFile('./page.jade', options);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-var html = compile(locals);
+var router = express.Router();
 
-console.log(html);
+router.route('/').get(function (req, res) {
+    var options = {};
+    var globals = {
+        content: 'Some substituted content.'
+    };
+
+    fs.readFile('./page.jade', 'utf8', function (err, data) {
+        var compileJade = jade.compile(data);
+
+        var locals = {
+            source: data,
+            html: compileJade(globals)
+        };
+
+        return res.render('editor', locals, function (err, data) {
+            return res.send(data);
+        });
+    });
+});
+
+app.use('/', router);
+
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + server.address().port);
+});
