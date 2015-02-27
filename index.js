7253fe5aa158b71aa09d85d2d9f20d63215e2205
Marshall Bowers <elliott.codes@gmail.com>
2015-02-26 19:24:17 -0500

    Separate combined line statements

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
 var router = express.Router();
 
